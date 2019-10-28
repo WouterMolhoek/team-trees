e9a3c9d14b73dc58cbb6833478415445927da5fd
Wouter Molhoek <WouterMolhoek@users.noreply.github.com>
2019-10-28 15:35:08 +0100

    Sava data to a json file 📄

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 import threading
+import json
 from selenium import webdriver
 
 chrome_path = r"C:\Users\woute\Desktop\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
+
+data = {}
+data['trees'] = []
 
 def setInterval(func,time):
     e = threading.Event()
@@ -10,7 +14,13 @@ def setInterval(func,time):
         func()
 
 def getAmount():
+    # Refresh the page
     driver.get("https://teamtrees.org/")
-    print(driver.find_element_by_id("totalTrees").text)
+    
+    data['trees'].append(driver.find_element_by_id("totalTrees").text)
+
+    # Write data to an JSON file
+    with open('data.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 setInterval(getAmount,10)
