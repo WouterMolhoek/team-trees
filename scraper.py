de8edd79e5b4a30915e610c2aba944d49584dd0c
Wouter Molhoek <WouterMolhoek@users.noreply.github.com>
2019-10-28 15:13:46 +0100

    Created an interval every 10 seconds to get the amount of trees 🕓

--- scraper.py
+++ scraper.py
@@ -1,10 +1,16 @@
+import threading
 from selenium import webdriver
 
 chrome_path = r"C:\Users\woute\Desktop\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 
-url = "https://teamtrees.org/"
-driver.get(url)
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
 
-print(driver.find_element_by_id("totalTrees").text)
+def getAmount():
+    driver.get("https://teamtrees.org/")
+    print(driver.find_element_by_id("totalTrees").text)
 
+setInterval(getAmount,10)
